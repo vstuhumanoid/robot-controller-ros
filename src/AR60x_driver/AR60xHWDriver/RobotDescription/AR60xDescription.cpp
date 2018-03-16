4b5525ff5e9aeb9654b8d1828842acca07d2bd97
Markov Alexey <markovalex95@gmail.com>
2018-03-16 17:09:03 +0300

    Project settings fixes  - Fix CMakeLists  - Add test code for driver

--- src/AR60x_driver/AR60xHWDriver/RobotDescription/AR60xDescription.cpp
+++ src/AR60x_driver/AR60xHWDriver/RobotDescription/AR60xDescription.cpp
@@ -4,13 +4,3 @@ AR60xDescription::AR60xDescription()
 {
 
 }
-
-std::map<int, SensorData> *AR60xDescription::getSensors()
-{
-    return &sensors;
-}
-
-std::map<int, JointData> *AR60xDescription::getJoints()
-{
-    return &joints;
-}
--- src/AR60x_driver/main.cpp
+++ src/AR60x_driver/main.cpp
@@ -1,17 +1,47 @@
 #include <ros/ros.h>
+#include <ros/package.h>
+#include <experimental/filesystem>
+#include <fstream>
+#include <string>
 #include "AR60xHWDriver/AR60xHWDriver.h"
+#include <QString>
 
+using namespace std;
+namespace fs = std::experimental::filesystem;
 
 int main(int argc, char** argv)
 {
+    QString wtf = QString::fromStdString("WTF");
+
     ros::init(argc, argv, "AR60x_driver");
     ros::NodeHandle nh;
 
     ros::Rate rate(1);
 
+    string path = ros::package::getPath("robot-controller-ros")+"/config.xml";
+    AR60xHWDriver driver;
+    driver.loadConfig(path);
+
+    driver.initPackets();
+    driver.robotConnect();
+
+    driver.SupplySetState(PowerData::Supply12V, true);
+    rate.sleep();
+    driver.SupplySetState(PowerData::Supply6V1, true);
+    driver.SupplySetState(PowerData::Supply6V2, true);
+    driver.SupplySetState(PowerData::Supply8V1, true);
+    driver.SupplySetState(PowerData::Supply8V2, true);
+    driver.SupplySetState(PowerData::Supply48V, true);
+    rate.sleep();
+
+
+
+
     while(ros::ok())
     {
-        ROS_INFO("Hello");
+        auto state = driver.PowerGetSupplyState(PowerData::Supply48V);
+        ROS_DEBUG("Voltage: %f\t Current: %f", state.Voltage, state.Current);
+
         ros::spinOnce();
         rate.sleep();
     }
